d24a671ce01b3605bc8e0b36d4a66bce495e8ea7
Naveen Kumar Reddy Murthi <mnreddy.nv@gmail.com>
2024-01-27 13:14:00 +0800

    implment sign in and signup using firebase

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,11 @@
 import React, { useState, useRef } from "react";
 import Header from "./Header";
-import {validateFormData} from "../utils/validate"
-
+import { validateFormData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
 
 const Login = () => {
   const [isSignInForm, setIsSignInForm] = useState(true);
@@ -14,9 +18,48 @@ const Login = () => {
   };
 
   const handleButtonClick = () => {
-    const errorMessage = validateFormData(email.current.value, password.current.value, name.current.value);
+    const errorMessage = validateFormData(
+      email.current.value,
+      password.current.value
+    );
     setErrorMessage(errorMessage);
-  }
+    if (errorMessage) return;
+
+    if (!isSignInForm) {
+      //signup logic
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const message = error.message;
+          setErrorMessage(errorCode + "-" + message);
+        });
+    } else {
+      //sign in logic
+
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log("logged in user=", user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const message = error.message;
+          setErrorMessage(errorCode + "-" + message);
+        });
+    }
+  };
 
   return (
     <div>
@@ -27,30 +70,45 @@ const Login = () => {
           alt="banner"
         />
       </div>
-      <form onSubmit={(e)=> e.preventDefault()}  className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80">
-        <h1 className="font-bold text-3xl py-4">{isSignInForm ? "Sign In": "Sing Up"}</h1>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white bg-opacity-80"
+      >
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sing Up"}
+        </h1>
         {!isSignInForm && (
-          <input ref={name}
+          <input
+            ref={name}
             type="text"
             placeholder="Full Name"
             className="p-4 my-4 w-full bg-gray-600"
           />
         )}
-        <input ref={email}
-          type="text" 
+        <input
+          ref={email}
+          type="text"
           placeholder="Email Address"
           className="p-4 my-4 w-full bg-gray-600"
         />
-        <input ref={password}
+        <input
+          ref={password}
           type="password"
           className="p-4 my-4 w-full bg-gray-600"
           placeholder="password"
         />
         <p className="text-red-500 font-bold">{errorMessage}</p>
-        <button className="p-4 my-4 bg-red-600 w-full rounded-lg" onClick={handleButtonClick}>
-        {isSignInForm ? "Sign In": "Sing Up"}
+        <button
+          className="p-4 my-4 bg-red-600 w-full rounded-lg"
+          onClick={handleButtonClick}
+        >
+          {isSignInForm ? "Sign In" : "Sing Up"}
         </button>
-        <p className="cursor-pointer" onClick={toggleSignInForm} >{isSignInForm ? "New to Netflix? SingUp Now" : "Already registered? Sign In Now."  }</p>
+        <p className="cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? SingUp Now"
+            : "Already registered? Sign In Now."}
+        </p>
       </form>
     </div>
   );
